06c031e976936edd6e95726a0def9218298dac42
Hadi Ahmadzadeh <hadi.wf77@gmail.com>
2026-01-30 22:30:49 +0330

    Feat(user): create model

--- user-service/main.py
+++ user-service/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from database import engine, Base
+from models import User
 
 app = FastAPI(title="User Service")
 
+
+@app.on_event("startup")
+async def startup():
+    # Create database tables
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
 @app.get("/")
 def root():
-    return {"service": "user-serviceee"}
+    return {"service": "user-service"}
 
---
+++ user-service/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime, timezone
+import uuid
+from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    main_role = Column(String(255), nullable=False)
+    full_name = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc), nullable=False)
+
